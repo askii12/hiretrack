42ea007c39f6983ac9045d6753a6c0f1c3779861
DeresiuPhoi <mussepovn@gmail.com>
2026-04-02 00:43:15 +0500

    Initialize HireTrack with backend, frontend, models, and auth setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,24 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "HireTrack API is running" });
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
